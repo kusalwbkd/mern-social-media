e1cb53a720578a04d434caecb8c3111ff19d034f
kuslwbkd <kusalwbkddhananjaya@gmail.com>
2024-07-11 01:17:55 +0530

    small bug fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -75,7 +75,7 @@ const App = () => {
             element:<NotificationPage/>,
             loader:notificationLoader,
             errorElement:<ErrorElement/>,
-            errorElement:<ErrorElement/>
+            
 
 
         },
